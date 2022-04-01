131b7286d2cf26f03719ca44ef533b45bf1debef
Craig Uffindall <craig.uffindall@apdcomms.co.uk>
2022-04-01 15:04:52 +0100

    Further changes

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from todo_app.data.session_items import get_items
 
+from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
