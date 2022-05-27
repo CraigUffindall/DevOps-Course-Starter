ab4ac155f36f963d5a9a570947d312a640fde1ff
Craig Uffindall <craig.uffindall@necsws.com>
2022-05-27 15:36:13 +0100

    Added some tests

--- todo_app/viewmodel.py
+++ todo_app/viewmodel.py
@@ -5,3 +5,24 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+
+    @property
+    def todo_items(self):
+        return self.get_items_by_status('To Do')
+
+    @property
+    def doing_items(self):
+        return self.get_items_by_status('Doing')
+
+    @property
+    def done_items(self):
+        return self.get_items_by_status('Done')
+
+    def get_items_by_status(self, status):
+        items = []
+        
+        for item in self._items:
+            if item.status == status:
+                items.append(item)
+
+        return items
